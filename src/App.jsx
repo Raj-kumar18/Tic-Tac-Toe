6b8bb7935980a198c4b817f1827a014e083a1ee5
Raj kumar <drajkumar01234@gmail.com>
2024-10-09 19:06:58 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Board from './tic-tac-toe/board'
+
+function App() {
+  
+
+  return (
+    <>
+     <Board/>
+    </>
+  )
+}
+
+export default App
